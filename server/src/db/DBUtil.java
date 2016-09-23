958302f17df32f4388ef28d116d4d74c0fd2d1e4
yvonnewei <1034082168@qq.com>
2016-09-22 22:46:43 -0400

    server/readme has been added one line

--- server/src/db/DBUtil.java
+++ server/src/db/DBUtil.java
@@ -2,7 +2,7 @@ package db;
 
 public class DBUtil {
 	private static final String HOSTNAME = "localhost";
-	private static final String PORT_NUM = "3306";
+	private static final String PORT_NUM = "8889";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
 	
